d5dcb0cd9ece3e548f604d6a726cc8dfce3600cf
Prakamya Bhuvi <prakamyabhuvi108@gmail.com>
2026-04-13 13:41:18 +0530

    fix: update cors for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,7 +25,10 @@ app.use(helmet());
 
 // 🌐 CORS
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: [
+    'http://localhost:3000',
+    process.env.FRONTEND_URL
+  ],
   credentials: true,
 }));
 
